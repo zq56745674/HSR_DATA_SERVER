05e322662fbbd26ccacea853b2fabc3201453ced
zq56745674 <zzm305808127@163.com>
2024-08-14 18:17:17 +0800

    init

---
+++ Login.py
@@ -0,0 +1,71 @@
+from PySide6.QtWidgets import QApplication, QWidget, QMessageBox
+from PySide6.QtGui import QIcon
+from Login_ui import Ui_Form
+from HSRMain import MainWindow
+import pymysql
+import logging
+import traceback
+
+# 设置日志
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class MyWindow(QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.bind()
+        self.db = None
+        
+    def bind(self):
+        self.pushButton.clicked.connect(self.login_function)
+
+    def get_database_connection(self, dbhost, dbuser, dbpass, dbname):
+        try:
+            if dbpass == "" or dbpass == None:
+                db = pymysql.connect(host=dbhost, user=dbuser, database=dbname)
+            else:
+                db = pymysql.connect(host=dbhost, user=dbuser, password=dbpass, database=dbname)
+            logging.info("数据库连接成功")
+            return db
+        except pymysql.Error as e:
+            logging.error("数据库连接失败：" + str(e))
+            return None
+    
+    def login_function(self):
+        # 获取文本框的内容
+        host = self.lineEdit.text()
+        username = self.lineEdit_2.text()
+        password = self.lineEdit_3.text()
+        dbname = self.lineEdit_4.text()
+        logging.info("host: %s, username: %s, password: %s, dbname: %s", host, username, password, dbname)
+        self.db = self.get_database_connection(host, username, password, dbname)
+        if self.db == None:
+            # 弹窗提示
+            msg_box = QMessageBox()
+            msg_box.setIcon(QMessageBox.Critical)
+            msg_box.setWindowTitle("错误")
+            msg_box.setText("连接失败")
+            msg_box.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            msg_box.button(QMessageBox.Ok).setText("继续")
+            msg_box.button(QMessageBox.Cancel).setText("取消")
+            result = msg_box.exec()
+
+            if result == QMessageBox.Cancel:
+                return
+        # 进入 Ui_HSRMainWindow
+        self.hide()
+        self.main_window = MainWindow(self.db)
+        self.main_window.show()
+    
+    def closeEvent(self, event):
+        if self.db:
+            self.db.close()
+            logging.error("数据库连接已关闭")
+        event.accept()
+
+if __name__ == "__main__":
+    app = QApplication()
+    app.setWindowIcon(QIcon('icon\HSR_HH.ico')) 
+    stats = MyWindow()
+    stats.show()
+    app.exec()
---
+++ Login.spec
@@ -0,0 +1,45 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['Login.py', 'Login_ui.py', 'HSRMain.py', 'HSRMain_ui.py'],
+    pathex=['D:\\pyTest\\sr_data_qt'],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=['PyQt5'],
+    noarchive=False,
+    optimize=0,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    [],
+    exclude_binaries=True,
+    name='Login',
+    debug=True,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    console=True,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    icon='icon\\HSR_HH.ico',
+)
+coll = COLLECT(
+    exe,
+    a.binaries,
+    a.datas,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    name='Login',
+)
