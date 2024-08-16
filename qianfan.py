d760fd375b221b4caf5f7648f879e152ffe2bfa5
zq56745674 <zzm305808127@163.com>
2024-08-16 17:34:22 +0800

    chore: Update qianfan.py to use secure authentication method

--- qianfan.py
+++ qianfan.py
@@ -3,8 +3,6 @@ import qianfan
 
 #【推荐】使用安全认证AK/SK鉴权，通过环境变量初始化认证信息
 # 替换下列示例中参数，安全认证Access Key替换your_iam_ak，Secret Key替换your_iam_sk
-os.environ["QIANFAN_ACCESS_KEY"] = "*"
-os.environ["QIANFAN_SECRET_KEY"] = "*"
 
 chat_comp = qianfan.ChatCompletion()
 
